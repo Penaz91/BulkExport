f1ee717db73bdd6bc315a5c32b536be59751c07e
BuildTools <penazarea@altervista.org>
2015-08-06 16:57:51 +0200

    Implemented Exportable serialization

--- src/main/java/BulkExport/BulkExport.java
+++ src/main/java/BulkExport/BulkExport.java
@@ -1,6 +1,11 @@
 package BulkExport;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
@@ -8,25 +13,72 @@ import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.yaml.snakeyaml.Yaml;
+
 public class BulkExport extends JavaPlugin{
 	static Inventory exportchest=null;
 	File Datafolder = getDataFolder();
+	static ArrayList<Exportable> items=new ArrayList<Exportable>();
 	@Override
     public void onEnable() {
 		getLogger().info("BulkExport has been enabled");
+		ConfigurationSerialization.registerClass(Exportable.class);
 		exportchest=Bukkit.getServer().createInventory(null, 54);
 		getServer().getPluginManager().registerEvents(new InventoryListener(), this);
+		/*items.add(new Exportable(1,new ItemStack(Material.STONE),64,1,new ItemStack(Material.PAPER),1));
+		items.add(new Exportable(2,new ItemStack(Material.CARROT_ITEM),64,1,new ItemStack(Material.PAPER),1));*/
+		/*Yaml yaml=new Yaml();
+		FileInputStream customfile=null;
+		try {
+			customfile = new FileInputStream(new File(getDataFolder(),"items.yml"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		items=new ArrayList((yaml.loadAs(customfile,java.util.LinkedHashMap.class)).keySet());*/
+		File customfile = new File(getDataFolder(),"items.yml");
+		FileConfiguration customyml=YamlConfiguration.loadConfiguration(customfile);
+		items=(ArrayList<Exportable>) customyml.get("0");
+		getLogger().info("Items: " +items);
     }
- 
     @Override
     public void onDisable() {
     	getLogger().info("BulkExport has been Disabled");
     	HandlerList.unregisterAll(this);
+    	File customfile = new File(getDataFolder(),"items.yml");
+		FileConfiguration customyml=YamlConfiguration.loadConfiguration(customfile);
+		/*Integer i=0;
+		for (Exportable item:items){
+			customyml.createSection(i.toString());
+			customyml.set(i.toString(),item);
+			i=i+1;
+		}*/
+		customyml.createSection("0");
+		customyml.set("0",items);
+		try{
+			customyml.save(customfile);
+		} catch (IOException e){
+			getLogger().info("Unable to save items.yml");
+		}
+		/*Map<String, Object> map=customyml.getValues(false);
+		if (map.get(args[0])==null){
+			customyml.createSection(args[0]);
+			customyml.set(args[0],i);
+		}else{
+			customyml.set(args[0], i);
+		}
+		try {
+			customyml.save(customfile);
+		} catch (IOException e) {
+			sender.sendMessage("Unable to save item, items.yml not found");*/
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -44,9 +96,6 @@ public class BulkExport extends JavaPlugin{
     }
     public static void Handle(Player player){
     	int full=0;
-    	Exportable Stone=new Exportable(1,new ItemStack(Material.STONE),64,1,new ItemStack(Material.PAPER),2);
-    	HashMap<Integer, ? extends ItemStack> mats=exportchest.all(Stone.getTrade().getType());
-    	Set<Integer> keys= mats.keySet();
     	ItemStack[] contents=exportchest.getContents();
     	HashMap<Integer, ItemStack> toreturn=new HashMap<Integer, ItemStack>();
     	boolean dirty=false;
@@ -65,8 +114,16 @@ public class BulkExport extends JavaPlugin{
     		}
     	}
     	if (!dirty){
+    		Exportable itemfound=null;
+    		for (Exportable item:items){
+    			if (item.getTrade().isSimilar(contents[0])){
+    				itemfound=item;
+    			}
+    		}
+    		HashMap<Integer, ? extends ItemStack> mats=exportchest.all(itemfound.getTrade().getType());
+    		Set<Integer> keys= mats.keySet();
     		for (Integer key:keys){
-    			if ((mats.get(key).isSimilar(Stone.getTrade()))&&(mats.get(key).getAmount()==Stone.getStackSize())){
+    			if ((mats.get(key).isSimilar(itemfound.getTrade()))&&(mats.get(key).getAmount()==itemfound.getStackSize())){
     				full=full+1;
     			}else{
     				toreturn.put(key, mats.get(key));
@@ -79,7 +136,7 @@ public class BulkExport extends JavaPlugin{
     				pi.addItem(toreturn.get(key));
     			}
     		}
-    		pi.addItem(new ItemStack(Stone.getTraded().getType(),full));
+    		pi.addItem(new ItemStack(itemfound.getTraded().getType(),full));
     	}
     	exportchest.clear();
     }
--- src/main/java/BulkExport/Exportable.java
+++ src/main/java/BulkExport/Exportable.java
@@ -1,29 +1,97 @@
 package BulkExport;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.inventory.ItemStack;
 
-public class Exportable {
+public class Exportable implements Serializable, ConfigurationSerializable {
+	private static final long serialVersionUID = 159754163929751635L;
 	int ID;
-	ItemStack item;
+	ItemStack trade;
 	int StackSize;
 	int NumStacks;
-	ItemStack trade;
+	ItemStack traded;
 	int NumTraded;
+	public Exportable(){
+		ID=0;
+		trade=null;
+		StackSize=0;
+		NumStacks=0;
+		traded=null;
+		NumTraded=0;
+	}
 	public Exportable(int identifier,ItemStack totrade,int size,int stacks, ItemStack Traded, int numreturn){
 		ID=identifier;
-		item=totrade;
+		trade=totrade;
 		StackSize=size;
 		NumStacks=stacks;
-		trade=Traded;
+		traded=Traded;
 		NumTraded=numreturn;
 	}
+	public int getID(){
+		return ID;
+	}
 	public ItemStack getTrade(){
-		return item;
+		return trade;
+	}
+	public int getNumStacks(){
+		return NumStacks;
 	}
 	public int getStackSize(){
 		return StackSize;
 	}
 	public ItemStack getTraded(){
-		return trade;
+		return traded;
+	}
+	public int getNumTraded(){
+		return NumTraded;
+	}
+	public void setID(int identifier){
+		this.ID=identifier;
+	}
+	public void setTrade(ItemStack totrade){
+		this.trade=totrade;
+	}
+	public void setStackSize(int Size){
+		this.StackSize=Size;
+	}
+	public void setNumStacks(int stacks){
+		this.NumStacks=stacks;
+	}
+	public void SetTraded(ItemStack itemtraded){
+		this.traded=itemtraded;
+	}
+	public void setNumTraded(int numreturn){
+		this.NumTraded=numreturn;
+	}
+	/*
+	private void readObject(ObjectInputStream stream) throws ClassNotFoundException, IOException{
+		stream.defaultReadObject();
+	}
+	private void writeObject(ObjectOutputStream stream) throws IOException{
+		stream.writeInt(ID);
+		stream.writeObject(item);
+		stream.writeInt(StackSize);
+		stream.writeInt(NumStacks);
+		stream.writeObject(trade);
+		stream.writeInt(NumTraded);
+		
+	}*/
+	@Override
+	public Map<String, Object> serialize() {
+		Map<String,Object> serial=new HashMap<String,Object>();
+		serial.put("ID", this.ID);
+		serial.put("trade", this.trade);
+		serial.put("StackSize", this.StackSize);
+		serial.put("NumStacks", this.NumStacks);
+		serial.put("traded", this.traded);
+		serial.put("NumTraded", this.NumTraded);
+		return serial;
+	}
+	public Exportable deserialize(Map <String,Object> input){
+		return new Exportable(Integer.parseInt(input.get("ID").toString()),(ItemStack) input.get("trade"),Integer.parseInt(input.get("StackSize").toString()),Integer.parseInt(input.get("NumStacks").toString()),(ItemStack) input.get("traded"),Integer.parseInt(input.get("NumTraded").toString()));
 	}
 }
